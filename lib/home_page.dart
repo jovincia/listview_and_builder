29a54375cade18cf7937e64827ff864f8d061cc4
Jovincia <assogbadanielle15@gmail.com>
2024-04-11 16:02:03 +0100

    add square

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,18 @@
 import "package:flutter/material.dart";
+import "package:listview_builder/square.dart";
 
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
+    final List post = [
+    'post 1',
+    'post 2',
+    'post 3',
+    'post 4',
+    'post 5',
+    'post 6',
+    'post 7',
+  ];
   
   @override
   Widget build(BuildContext context) {
@@ -14,6 +24,17 @@ class HomePage extends StatelessWidget {
           
 
           //Instagram posts
+
+          Expanded(
+            child: Scaffold(
+                body: ListView.builder(
+                    itemCount: post.length,
+                    itemBuilder: (context, index) {
+                      return MySquare(
+                        child: post[index],
+                      );
+                    })),
+          ),
 
          
         ],
