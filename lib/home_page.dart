10ca794b230a33831d399d06ed125a584e8a76ad
Jovincia <assogbadanielle15@gmail.com>
2024-04-11 15:45:13 +0100

    first commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,29 +1,9 @@
 import "package:flutter/material.dart";
-import "package:listview_builder/circle.dart";
-import "package:listview_builder/square.dart";
+
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
-  final List post = [
-    'post 1',
-    'post 2',
-    'post 3',
-    'post 4',
-    'post 5',
-    'post 6',
-    'post 7',
-  ];
-
-  final List stories = [
-    'story 1',
-    'story 2',
-    'story 3',
-    'story 4',
-    'story 5',
-    'story 6',
-    'story 7',
-  ];
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,30 +11,11 @@ class HomePage extends StatelessWidget {
         children: [
           //Instagram stories
 
-          SizedBox(
-            height: 130,
-            child: Expanded(
-              child: ListView.builder(
-                  itemCount: stories.length,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return MyCircle(child: stories[index]);
-                  }),
-            ),
-          ),
+          
 
           //Instagram posts
 
-          Expanded(
-            child: Scaffold(
-                body: ListView.builder(
-                    itemCount: post.length,
-                    itemBuilder: (context, index) {
-                      return MySquare(
-                        child: post[index],
-                      );
-                    })),
-          ),
+         
         ],
       ),
     );
